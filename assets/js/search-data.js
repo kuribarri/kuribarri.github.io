3037064c1be3afa844e381596065cbec7c7aa785
kuribarri <kepauribarri@yahoo.es>
2025-01-12 12:57:16 +0000

    Deploying to gh-pages from @ kuribarri/kuribarri.github.io@a94af03650c9f26c74898c4b5d002c70af9df822 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -100,7 +100,7 @@ ninja.data = [{
       
         title: "El Hayedo de Otzarreta. Un rincón mágico en Bizkaia",
       
-      description: "ubicado en el Parque Natural de Gorbeia, es uno de los bosques más icónicos de Bizkaia. Considerado uno de los bosques más bellos del mundo por National Geographic, Otzarreta destaca por sus hayas centenarias y su atmósfera mágica.",
+      description: "Ubicado en el Parque Natural de Gorbeia, es uno de los bosques más icónicos de Bizkaia. Considerado uno de los bosques más bellos del mundo por National Geographic, Otzarreta destaca por sus hayas centenarias y su atmósfera mágica.",
       section: "Posts",
       handler: () => {
         
