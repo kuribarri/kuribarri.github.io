a2a9628c59f56a2b878de14f29f2629a9562f462
kuribarri <kepauribarri@yahoo.es>
2025-01-19 14:31:35 +0000

    Deploying to gh-pages from @ kuribarri/kuribarri.github.io@7bd095ec949309d68e138bd69232df5a7a60dc38 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,6 +25,13 @@ ninja.data = [{
           },
         },{id: "nav-sobre-mi",
           title: "Sobre mi",
+          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/cv/";
+          },
+        },{id: "nav-sobre-mi",
+          title: "Sobre mi",
           description: "Sobre el autor",
           section: "Navigation",
           handler: () => {
