b66a0f3fe572179c79d3b98a5d812bc59c10dc4a
kuribarri <kepauribarri@yahoo.es>
2026-03-14 23:28:47 +0000

    Deploying to gh-pages from @ kuribarri/kuribarri.github.io@c72cc9c096aa2381cae6513e5e59ef0018e8f594 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -34,7 +34,7 @@ ninja.data = [{
       
         title: "Entre el “hater” premium y el “fanboy” chino, reflexiones antes de sacar la...",
       
-      description: "Os enseño mi flujo de trabajo cuando tengo que catalogar y editar imágenes sin acabar por saturarse o colapsar cuando son de miles de fotografías",
+      description: "¿Es tan necesario gastar miles de euros en equipamiento cuando realmente se puede gastar menos “obteniendo lo mismo”? Una reflexión para todo fotógrafo, no solo amateurs de poca monta como un servidor o amateurs avanzados, sino también profesionales.",
       section: "Posts",
       handler: () => {
         
